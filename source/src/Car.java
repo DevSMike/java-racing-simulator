4771e345d2b5a72b6db5d98cfd89d9bedd144aab
DevSMike <likashev.migha@yandex.ru>
2022-10-15 16:10:47 +0300

    creating a car-racing game by technical specification

---
+++ source/src/Car.java
@@ -0,0 +1,14 @@
+class Car {
+    double maxSpeed; // максимальная скорость
+    float acceleration; // ускорение
+    int score; // количество очков
+    double kilometersTravelled; // количество пройденных километров
+    Integer nitroLevel; // уровень закиси азота
+
+    public Car(double maxSpeed, float acceleration, int score, Integer nitroLevel) {
+        this.maxSpeed = maxSpeed;
+        this.acceleration = acceleration;
+        this.score = score;
+        this.nitroLevel = nitroLevel;
+    }
+}
